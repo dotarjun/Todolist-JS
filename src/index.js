78c96f4b96574a86ff463c6b9d50816b4a3fa862
Arjun <arjunsingh8112@gmail.com>
2022-09-27 10:45:58 +0530

    📋 save form data in Array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "nes.css/css/nes.min.css";
 import './style.css';
 
+const form = document.querySelector('form');
+const fd = [];
+
+form.addEventListener('submit', (e) => {
+    const data = new URLSearchParams(new FormData(form).entries());
+    fd.push(data)
+    e.preventDefault();
+})
