7cf00c4a61e4c89415973f4de2a5f9af89f45e85
Arjun <arjunsingh8112@gmail.com>
2022-09-28 12:16:31 +0530

    🤫 date fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const form = document.querySelector('form');
 const modal = document.querySelector('dialog')
-const saveTodo = form.querySelector('.save')
+// const saveTodo = form.querySelector('.save')
 const cancelToDo = form.querySelector('.cancel')
 const fd = [
     {
@@ -15,14 +15,14 @@ const fd = [
     {
         title: "Demo Todo 2",
         description: "This is some more demo description. Blah blah blah",
-        "due": "2022-09-28", 
+        "due": "2022-09-28",
         priority: "1"
     },
 
 ];
 
 
-saveTodo.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const data = Object.fromEntries(new URLSearchParams(new FormData(form).entries()));
     fd.push(data)
@@ -56,7 +56,7 @@ const addToDotoDOM = () => {
         h2.textContent = ele.title
         desc.textContent = ele.description
         priority.textContent = `Priority ${ele.priority}`
-        due.textContent = ele.due
+        due.textContent = dateConverter(ele);
         info.appendChild(priority)
         info.appendChild(due)
         todoCard.appendChild(h2)
@@ -66,4 +66,21 @@ const addToDotoDOM = () => {
         console.log(fd);
     });
 }
+
+const dateConverter = (obj) => {
+    // "due": "2022-09-28",
+    const date = obj.due;
+    //  date = 2002-09-28
+    const dateArr = date.split('-')
+    // date dateArr = [2022,09,28]
+    const year = dateArr[0]
+    const month = dateArr[1]
+    const day = dateArr[2]
+    const newDate = `${day}/${month}`
+    return newDate;
+}
+
+console.log(dateConverter(fd[0]))
+
+
 addToDotoDOM();
